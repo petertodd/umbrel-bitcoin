263b2e5364e65f869ee3ad54b42f796db838c088
nmfretz <nmfretz@gmail.com>
2024-11-19 15:48:48 +1000

    Add rpcport to test script

--- test.sh
+++ test.sh
@@ -13,5 +13,5 @@ while true
 do
     sleep 10
     echo "Generating a block..."
-    docker-compose exec bitcoind bitcoin-cli -regtest generatetoaddress 1 "bcrt1qs758ursh4q9z627kt3pp5yysm78ddny6txaqgw"
+    docker-compose exec bitcoind bitcoin-cli -regtest -rpcport=8332 generatetoaddress 1 "bcrt1qs758ursh4q9z627kt3pp5yysm78ddny6txaqgw"
 done
